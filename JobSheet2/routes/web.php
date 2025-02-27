25b88c6e54382bf140c6d9c2e0670c6079a06f87
Ndaa01 <ananda.idn12345@gmail.com>
2025-02-27 22:36:51 +0700

    Post dan Comment

--- JobSheet2/routes/web.php
+++ JobSheet2/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user/{name}', function ($name) {
-    return 'Nama Saya '.$name;
-    });
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
