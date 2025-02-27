7a592d03993703fb9d6eae38dcaa6bfbc5687bb4
Ndaa01 <ananda.idn12345@gmail.com>
2025-02-27 22:32:30 +0700

    Nama

--- JobSheet2/routes/web.php
+++ JobSheet2/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/about', function () {
-    return 'NIM: 2341720132';
-});
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+    });
